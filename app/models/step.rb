1f6c15ed82efca30b7abb8897015aa012d5254b9
JamisonOrdway <jamisonordway@gmail.com>
2018-09-10 19:31:45 -0600

    Removes unnecessary validations and validation tests for step model

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,15 +1,6 @@
 class Step < ApplicationRecord
   belongs_to :possible_route
-  validates :headsign, presence: true
-  validates :arrival_time, presence: true
-  validates :departure_time, presence: true
-  validates :arrival_stop, presence: true
-  validates :departure_stop, presence: true
-  validates :name, presence: true
-  validates :short_name, presence: true
   validates :distance, presence: true
   validates :duration, presence: true
-  validates :color, presence: true
-  validates :num_stops, presence: true
 
 end
