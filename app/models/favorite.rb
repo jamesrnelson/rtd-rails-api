fba4511c4d11a2bd285b5cd6ce5de882773afc4b
JNelson <nelson.jimmy@gmail.com>
2018-09-12 09:16:58 -0600

    Removes favorite model and favorite spec.

--- app/models/favorite.rb
+++
@@ -1,7 +0,0 @@
-class Favorite < ApplicationRecord
-  belongs_to :user
-  belongs_to :itinerary
-  validates :user_id, presence: true
-  validates :itinerary_id, presence: true
-  validates :title, presence: true
-end
--- spec/models/favorites_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Favorite, type: :model do
-  it { should belong_to :user}
-  it { should belong_to :itinerary}
-  it { should validate_presence_of :user_id }
-  it { should validate_presence_of :itinerary_id }
-  it { should validate_presence_of :title }
-end
