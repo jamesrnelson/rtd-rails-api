b17fd883b074df9e5b38f08b3d433a63f1d35c92
JNelson <nelson.jimmy@gmail.com>
2018-09-12 13:12:28 -0600

    Adds itinerary relationship to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,4 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-<<<<<<< HEAD
-  validates :email, presence: true
-  validates :firebase_id, presence: true
-  validates :username, presence: true
-
-=======
-<<<<<<< HEAD
->>>>>>> Resolves merge conflict in schema
-=======
->>>>>>> resolves merge conflicts in user model
   has_many :itineraries
   validates :email, presence: true
   validates :uid, presence: true
