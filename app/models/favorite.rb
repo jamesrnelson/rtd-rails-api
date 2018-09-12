fba9c2f6fa6cbabd4ac42d70f2779d405368d30a
JNelson <nelson.jimmy@gmail.com>
2018-09-12 07:04:19 -0600

    Adds itinerary model and updates favorite and possible_route models.

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,9 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user
-  has_many :possible_routes
+  belongs_to :itinerary
   validates :user_id, presence: true
+  validates :itinerary_id, presence: true
   validates :title, presence: true
-  validates :start_address, presence: true
-  validates :end_address, presence: true
-
 end
---
+++ app/models/itinerary.rb
@@ -0,0 +1,7 @@
+class Itinerary < ApplicationRecord
+  belongs_to :user
+  has_many :possible_routes
+  validates :user_id, presence: true
+  validates :start_address, presence: true
+  validates :end_address, presence: true
+end
