a4706148d1f2f068a457ea95f8456bab8ae08b37
JamisonOrdway <jamisonordway@gmail.com>
2018-09-13 15:08:18 -0600

    Resolves save conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
+<<<<<<< HEAD
   validates :email, presence: true
   validates :firebase_id, presence: true
   validates :username, presence: true
 
+=======
+<<<<<<< HEAD
+>>>>>>> Resolves merge conflict in schema
   has_many :itineraries
 end
+=======
+  validates :email, presence: true
+  validates :firebase_id, presence: true
+  validates :username, presence: true
+end
+>>>>>>> Adds email, firebase_id, and username to User model
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2018_09_13_184239) do
-=======
-ActiveRecord::Schema.define(version: 2018_09_12_204008) do
->>>>>>> Adds columns to step to give front end more information to work with.
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
