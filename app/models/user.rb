afa69aa5ce00bcb38d4a368c7a90886c2cbf6bee
JamisonOrdway <jamisonordway@gmail.com>
2018-09-13 15:03:40 -0600

    fixes merge conflict in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+<<<<<<< HEAD
+=======
+  validates :email, presence: true
+  validates :firebase_id, presence: true
+  validates :username, presence: true
+
+>>>>>>> fixes merge conflict in user
   has_many :itineraries
   validates :email, presence: true
   validates :uid, presence: true
