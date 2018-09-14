a32b2ea64ce3ee7f0cfdaade186c5e73dbb3a4cf
JNelson <nelson.jimmy@gmail.com>
2018-09-14 16:30:06 -0600

    Adds user migration for username. Renders the user after creation.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_09_13_204245) do
-
+ActiveRecord::Schema.define(version: 2018_09_14_222832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +63,7 @@ ActiveRecord::Schema.define(version: 2018_09_13_204245) do
     t.datetime "updated_at"
     t.string "email"
     t.string "uid"
+    t.string "username"
   end
 
   add_foreign_key "itineraries", "users"
