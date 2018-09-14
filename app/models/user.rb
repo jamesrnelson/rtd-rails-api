b4e1cb6030e11295694dbd55b39dcfed17ab1592
JNelson <nelson.jimmy@gmail.com>
2018-09-14 06:55:37 -0600

    Adds has_many possible routes through itineraries to User model to enable us to render user.possible_routes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :itineraries
+  has_many :possible_routes, through: :itineraries
   validates :email, presence: true
   validates :uid, presence: true
 end
