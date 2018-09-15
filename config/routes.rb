3ad3bfc18cb80f82f3617992e50375da5bc09752
JNelson <nelson.jimmy@gmail.com>
2018-09-15 13:09:06 -0600

    Adds route and controller action to reflect the user show action which will enable login.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/users', to: 'users#create'
+      get '/users/:uid', to: 'users#show'
       namespace :users do
         get '/:id/favorite_itineraries', to: 'favorite_itineraries#index'
         get '/:id/itineraries', to: 'itineraries#index'
