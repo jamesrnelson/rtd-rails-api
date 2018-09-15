d189e9a62550eb4368e54bcf90a368e062565ab0
JamisonOrdway <jamisonordway@gmail.com>
2018-09-15 17:02:18 -0600

    cleans up test files

--- spec/requests/api/v1/favorite_itineraries/user_can_get_favorite_itinerary_spec.rb
+++ spec/requests/api/v1/favorite_itineraries/user_can_get_favorite_itinerary_spec.rb
@@ -31,7 +31,6 @@ describe "GET /api/v1/users/:uid/favorites/:favorite_id" do
 
     expect(response).to be_successful
     favorite = JSON.parse(response.body, symbolize_names: true)
-    # binding.pry
     expect(favorite[:id]).to eq(fav_itinerary_2.id)
     expect(favorite[:title]).to eq(fav_itinerary_2.title)
   end
