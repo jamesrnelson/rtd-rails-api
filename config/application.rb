54ce8a30494a761d4efd28e2accbe5bac606452f
JamisonOrdway <jamisonordway@gmail.com>
2018-09-24 09:43:44 -0600

    adds delete method to cors config in application.rb

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module RtdRailsApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :options]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :options, :delete]
       end
     end
 
