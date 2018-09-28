2f73449c945e37c6f81511a272b4e09817ae81ad
JNelson <nelson.jimmy@gmail.com>
2018-09-28 14:04:33 -0600

    Adds head method.

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module RtdRailsApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head]
       end
     end
   end
